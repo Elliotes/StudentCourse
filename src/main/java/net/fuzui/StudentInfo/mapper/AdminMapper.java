0a568ee00fd66f7ab094f1db248cc9e81f93b10f
Elliotes <1223512829@qq.com>
2020-07-17 15:25:33 +0800

    test

---
+++ src/main/java/net/fuzui/StudentInfo/mapper/AdminMapper.java
@@ -0,0 +1,24 @@
+package net.fuzui.StudentInfo.mapper;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @ProjectName: StudentInfo
+ * @Package: net.fuzui.StudentInfo.mapper
+ * @ClassName: AdminMapper
+ * @Description: 管理员数据访问层接口
+ * @Author: cwl
+ * @UpdateRemark: 新建
+ * @Version: 1.0
+ */
+public interface AdminMapper {
+
+    /**
+     *  管理员登录设置
+     * @param aname   管理员账号（唯一）
+     * @param apassword   密码
+     * @return
+     */
+    public String queryByNamePwd(@Param("aname") String aname, @Param("apassword") String apassword);
+
+}
